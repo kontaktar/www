20eb418d016ebcddde98f1b4e5edaff78d6e5fe3
Einar Alexander Eymundsson @einar <einarae@icelandair.is>
2019-10-05 03:10:42 +0000

    This should work

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 const withPlugins = require("next-compose-plugins");
 const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
@@ -6,9 +10,9 @@ const withFonts = require("next-fonts");
 module.exports = withPlugins([[withCSS], [withSass], [withFonts]], {
   // target: 'serverless',
   // ssr: true,
-  env: {
-    API_URL: process.env.API_URL || "http://localhost:3000"
-  },
+  // env: {
+  //   API_URL: process.env.API_URL || "http://localhost:3000"
+  // },
   enableSvg: true,
   cssModules: true,
   cssLoaderOptions: {
@@ -16,6 +20,13 @@ module.exports = withPlugins([[withCSS], [withSass], [withFonts]], {
     localIdentName: "[local]___[hash:base64:5]"
   },
   webpack(config, { dev, isServer }) {
+    config.plugins.push(
+      // Read the .env file
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true
+      })
+    );
     if (dev) {
       config.module.rules.push({
         test: /\.js$/,
