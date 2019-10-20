6307931d02b54d3a665351551aa9969b959a1953
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2019-10-20 19:17:57 +0000

    Added pure-react-carousel

--- src/Components/index.js
+++ src/Components/index.js
@@ -8,3 +8,4 @@ export { default as Icon } from "./Icon";
 export { default as Navigation } from "./Navigation";
 export { default as SearchBar } from "./SearchBar";
 export { default as Logo } from "./Logo";
+export { default as Carousel } from "./Carousel";
