4579b66752cc416e7318ef1fc8077973e29de747
Einar Alexander Eymundsson @einar <einarae@icelandair.is>
2020-04-26 21:06:21 +0000

    Cleaned up subscription page

--- src/pages/subscription.js
+++ src/pages/subscription.js
@@ -1,28 +1,12 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
 import React from "react";
-// import PropTypes from "prop-types";
-// eslint-disable-next-line no-unused-vars
-// import { useAuth } from "utils/auth";
-// eslint-disable-next-line no-unused-vars
 import { MainLayout, SubscriptionContainer } from "layouts";
 
-const Subscription = ({ searchInput }) => {
-  // const { isLoggedIn } = useAuth();
+const Subscription = () => {
   return (
     <MainLayout>
       <SubscriptionContainer />
     </MainLayout>
   );
 };
-
-Subscription.getInitialProps = (ctx) => {
-  const { searchInput } = ctx.query;
-  return { searchInput };
-};
-
-// Search.propTypes = {
-//   data: PropTypes.object.isRequired
-// };
 
 export default Subscription;
