5705ade244bb0e5755102329e106665f468ef81b
Einar Alexander Eymundsson @einar <einarae@icelandair.is>
2020-05-12 22:42:04 +0000

    Database WIP

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,10 +1,7 @@
 const database = require("../../../utils/database").instance;
 
-export default async (request, response) => {
+export default async ({ query: { id } }, response) => {
   try {
-    const {
-      query: { id }
-    } = request;
     const post = await database.one("SELECT * FROM users WHERE id=$1", id);
     response.status(200).json(post);
   } catch (error) {
