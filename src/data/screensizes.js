0e226e9e7a36f1d09af5dff55ac1fd9bbae7d91a
Einar Alexander Eymundsson @einar <einarae@icelandair.is>
2020-05-14 22:27:21 +0000

    Screensize file missing all along

---
+++ src/data/screensizes.js
@@ -0,0 +1,14 @@
+export default {
+  // this file is temporary, had some better setup on my home computer
+  tabletsPortrait: 768,
+  tabletsLandscape: 992,
+  default: 1024,
+  desktopLarge: 1200
+};
+
+/*
+smaller than or equal to 768 px (smartphones)
+larger than 768 px (small devices, tablets)
+larger than 992 px (medium devices)
+larger than 1200px (large devices)
+*/
