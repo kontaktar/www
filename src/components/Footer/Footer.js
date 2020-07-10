880242a28d0a65f04b5a69d85a81087b7f9bc7b6
Einar Alexander Eymundsson @einar <einarae@icelandair.is>
2020-07-10 01:43:08 +0000

    wip styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Icon, Logo } from "components";
+import useMaxWidth from "hooks/useMaxWidth";
 import styles from "./Footer.module.scss";
 
 const Footer = ({ className }) => {
   return (
-    <footer className={`${styles.footer} ${className}`}>
-      <Logo className={styles.logo} withTitle inverted />
-      <Icon name="phone" />
-      <p>444 4444</p>
-      <Icon name="email" />
-      <p>kontaktar@kontaktar.is</p>
+    <footer {...useMaxWidth()}>
+      <div className={`${styles.footer} ${className}`}>
+        <Logo className={styles.logo} withTitle inverted />
+        <Icon name="phone" />
+        <p>444 4444</p>
+        <Icon name="email" />
+        <p>kontaktar@kontaktar.is</p>
+      </div>
     </footer>
   );
 };
