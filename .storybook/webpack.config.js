e87ebb82c8f317313f5d13dd4faeb7ca09ecf40d
Einar Alexander Eymundsson @einar <einaralex@gmail.com>
2020-10-03 16:10:54 +0000

    Storybook builds - but have to take a lookt at this, some stuff is broken

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,40 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
+
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.scss$/,
-    use: [
-      { loader: "style-loader" },
-      {
-        loader: "css-loader",
-        options: {
-          modules: true,
-          localIdentName: "[local]___[hash:base64:5]"
-        }
-      },
-      { loader: "sass-loader" }
+  config.resolve = {
+    extensions: [".ts", ".tsx", ".js"],
+    plugins: [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve("./tsconfig.json"),
+        logLevel: "info",
+        extensions: [".ts", ".tsx", ".js"]
+      })
     ]
-  });
+  };
+  config.module.rules.push(
+    {
+      test: /\.scss$/,
+      use: [
+        { loader: "style-loader" },
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            localIdentName: "[local]___[hash:base64:5]"
+          }
+        },
+        { loader: "sass-loader" }
+      ]
+    },
+    {
+      test: /\.ts$|tsx/,
+      exclude: /^node_modules/,
+      loader: "ts-loader",
+      options: {
+        configFile: path.resolve("./tsconfig.json")
+      }
+    }
+  );
   return config;
 };
