cce3f2f5f09cd51040955b8dbf1833817f9de0c8
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2020-11-18 23:11:19 +0000

    remove unused + cleanup

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 /* eslint-disable no-param-reassign */
 require("dotenv").config({ path: ".env" });
 
-const withPlugins = require("next-compose-plugins");
-// const withCSS = require("@zeit/next-css");
-// const withSass = require("@zeit/next-sass");
-const withFonts = require("next-fonts");
-
-module.exports = withPlugins([[withFonts]], {
+module.exports = {
   target: "serverless",
   // publicRuntimeConfig: false,
   ssr: true,
@@ -16,11 +11,6 @@ module.exports = withPlugins([[withFonts]], {
     SECRET_COOKIE_PASSWORD: "THIS_HAS_TO_BE_AT_LEAST_32_LETTER_LONG" // TODO: host at VERCEL (and change ofcourse)
   },
   enableSvg: true,
-  // cssModules: true,
-  // cssLoaderOptions: {
-  //   importLoaders: 1,
-  //   localIdentName: "[local]___[hash:base64:5]"
-  // },
   webpack(config, { dev, isServer, webpack }) {
     if (dev) {
       config.module.rules.push({
@@ -35,4 +25,4 @@ module.exports = withPlugins([[withFonts]], {
     config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
     return config;
   }
-});
+};
--- src/layouts/FrontPageContainer/FrontPageContainer.js
+++ src/layouts/FrontPageContainer/FrontPageContainer.js
@@ -10,53 +10,15 @@ import { useRouter } from "next/router";
 import { useStore } from "react-redux";
 import cx from "classnames";
 import { Button, Carousel, SearchBar } from "components";
-import { breakPointSettings } from "components/Carousel/Carousel";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import styles from "./FrontPageContainer.module.scss";
 
 const FrontPageContainer = () => {
-  const heroRef = React.useRef(null);
   const router = useRouter();
   const { width: windowWidth } = useWindowDimensions();
   const [searchInput, setSearchInput] = useState(undefined);
-  // const [frameWidth, setFrameWidth] = useState(undefined);
 
   const store = useStore();
-
-  // let observer;
-  // if (process.browser) {
-  //   // client side only
-  //   // eslint-disable-next-line react-hooks/rules-of-hooks
-  //   observer = React.useRef(
-  //     new ResizeObserver((entries) => {
-  //       // Only care about the first element, we expect one element to be watched
-  //       const { width } = entries[0].contentRect;
-  //       const padding = 96;
-  //       if (
-  //         breakPointSettings.find((s) => width + padding >= s.breakpoint) &&
-  //         breakPointSettings.find((s) => width + padding >= s.breakpoint)
-  //           .breakpoint !== frameWidth
-  //       ) {
-  //         setFrameWidth(
-  //           breakPointSettings.find((s) => width + padding >= s.breakpoint)
-  //             .breakpoint
-  //         );
-  //       }
-  //     })
-  //   );
-  // }
-
-  // React.useEffect(() => {
-  //   if (heroRef.current) {
-  //     observer.current.observe(heroRef.current);
-  //   }
-
-  //   return () => {
-  //     observer.current.unobserve(heroRef.current);
-  //   };
-  // }, [heroRef, observer]);
-
-  // React.useEffect(() => console.log("framewidht", windowWidth), [windowWidth]);
 
   const onSearchBarInput = (event) => {
     setSearchInput(event.target.value);
@@ -73,12 +35,13 @@ const FrontPageContainer = () => {
 
   const onSearchSubmit = () => {
     console.log(searchInput);
+    // TODO:
   };
   // TODO: User should be able to enter something in the input box, then push enter to submit search.
 
   return (
     <div className={styles.frontpage}>
-      <div className={styles.hero} ref={heroRef}>
+      <div className={styles.hero}>
         <div className={styles.search_section}>
           <h1>
             Ertu að leita að{" "}
