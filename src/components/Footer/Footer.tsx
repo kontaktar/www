30e7c82857a08b203afcf278670e64c6bd62382a
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2021-02-25 23:26:58 +0000

    fix footer. ADDED OVERFLOW:HIDDEN to UserLayout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,24 +16,25 @@ const Footer = ({ className, userLayout }: Props): ReactElement => {
   return (
     <div className={styles.root}>
       <footer {...maxWidth} className={cx(styles.footer, className)}>
-        <div className={styles.logo_wrapper}>
-          {!userLayout && (
-            <>
-              {width < screensizes.default ? (
-                <>
-                  {width > screensizes.tabletsPortrait ? (
-                    <Logo className={styles.logo} inverted />
-                  ) : null}
-                </>
-              ) : (
-                <>
-                  <Logo className={styles.logo} withTitle inverted />
-                </>
-              )}
-            </>
-          )}
-        </div>
         <div className={styles.info}>
+          <div className={styles.logo_wrapper}>
+            {!userLayout && (
+              <>
+                {width < screensizes.default ? (
+                  <>
+                    {width > screensizes.wideSmartphone ? (
+                      <Logo className={styles.logo} inverted />
+                    ) : null}
+                  </>
+                ) : (
+                  <>
+                    <Logo className={styles.logo} withTitle inverted />
+                  </>
+                )}
+              </>
+            )}
+          </div>
+
           <div className={styles.row}>
             <Icon className={styles.icons} name="phone" />
             <p>862-9580</p>
