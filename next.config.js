8284821b77af85f74391cbcda708a098660abd91
einaralex <einaralex@gmail.com>
2022-08-12 20:33:52 +0000

    try fix build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 require("dotenv").config({ path: ".env" });
 
 module.exports = {
-  webpack5: true,
-  // publicRuntimeConfig: false,
-  serverless: false,
-  ssr: true,
   env: {
     BASE_URL: process.env.VERCEL_URL || process.env.LOCALHOST,
     FIREBASE_EMULATOR: process.env.FIREBASE_EMULATOR || "0",
@@ -15,11 +11,5 @@ module.exports = {
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_DATABASE_URL: `${process.env.FIREBASE_PROJECT_ID}.firebase.io`
   },
-  enableSvg: true,
-  webpack(config, { dev, isServer, webpack }) {
-    // config.plugins.push(
-    //   new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
-    // );
-    return config;
-  }
+  enableSvg: true
 };
